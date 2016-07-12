58371f0d071f2916e2292a7042866a74f5d5980a
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2016-07-12 21:20:00 +0200

    Pas de config du nom

--- bin/bot.js
+++ bin/bot.js
@@ -4,12 +4,10 @@ var Boteoz = require('../lib/boteoz');
 
 var token = process.env.BOT_API_KEY;
 var dbPath = process.env.BOT_DB_PATH;
-var name = process.env.BOT_NAME;
 
 var boteoz = new Boteoz({
     token: token,
-    dbPath: dbPath,
-    name: name
+    dbPath: dbPath
 });
 
 boteoz.run();
